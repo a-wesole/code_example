15e5cb5b477899f838edf4fc6d3be1d2e57f5d51
a-wesole <awesole@purdue.edu>
2023-02-21 10:07:08 -0500

    added files to Ntuple

--- c++/loop_via_ifstream.C
+++ c++/loop_via_ifstream.C
@@ -1,8 +1,17 @@
-//////
+/////,
+#include <iostream>
+using namespace std;
+
 void loop_via_ifstream(int istart, int iend)
 {
+  
   ifstream file_stream("file.list");
-  int nfile_tot = 2;
+  string output_name = "nt"+std::to_string(istart)+"_"+std::to_string(iend)+".root";
+  cout << output_name << endl;  
+
+  TFile results("results.root", "recreate");
+  TNtuple *b = new TNtuple("output_name", "Files & Numbers", "number");
+  b->Print();
 
   string filename;
   int ifile = 0;
@@ -21,25 +30,29 @@ void loop_via_ifstream(int istart, int iend)
     if(ifile >= iend)
       break;
 
+    b->Fill(ifile);
     cout << "file  = " << ifile<<" name = "<<filename <<endl;
 
     //____ your analysis code
-    TFile fin(filename.c_str());
+    //TFile fin(filename.c_str());
 
-    if(fin.IsZombie()) {
-      ifile ++;   // you need this here to keep track of the file
-      continue;
-    }
+    //if(fin.IsZombie()) {
+    //  ifile ++;   // you need this here to keep track of the file
+    //  continue;
+    //}
 
-    //TTree * tree = (TTree*)fin.Get("DecayTree");
+    ////TTree * tree = (TTree*)fin.Get("DecayTree");
 
-    string output_name = "nt"+std::to_string(istart)+"_"+std::to_string(iend)+".root";
     cout<<" output_name: "<<output_name<<endl;
-
-    fin.Close();
+    //fin.Close();
     //_____ end of your analysis code
 
     //
     ifile++;
   }
+  results.cd();
+  b->Write();
+  results.Close();
+
 }
+
